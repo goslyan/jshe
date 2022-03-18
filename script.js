84e077faeccff2975c492d2434e531e3fd2f9d2b
goslyan <90595997+goslyan@users.noreply.github.com>
2022-03-18 11:59:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+console.log("hehe, this console log gagaga")
+let title = prompt('Как называется ваш проект?', 'jshe');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+let rollback = Math.round(Math.random() * 30);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice;
+let allServicePrices;
+
+let mas = screens.split('');
+
+
+
+
+const getAllServicePrices = function (servicePrice1, servicePrice2) {
+    return servicePrice1 + servicePrice2
+}
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2)
+
+
+
+function getFullPrice(fullPrice) {
+    return screenPrice + allServicePrices
+}
+getFullPrice()
+
+
+
+const getServicePercentPrices = function (fullPrice, rollback) {
+    return fullPrice - rollback
+}
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback)
+
+
+
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable)
+}
+
+
+
+const getRollbackMessage = function () {
+    if (fullPrice >= 30000) {
+        return 'Даём скидочку 10%'
+    } else if (fullPrice >= 15000 & fullPrice < 30000) {
+        return 'Даём скидочку 5%'
+    } else if (fullPrice < 15000 & fullPrice >= 0) {
+        return 'Скидка не предусмотрена'
+    } else if (fullPrice <= 0) {
+        return 'Что-то пошло не так'
+    };
+}
+
+
+
+console.log('экраны:' + " " + screens.toLowerCase())
+console.log('стоимость за вычетом процента отката посреднику' + " " + servicePercentPrice);
+console.log(getRollbackMessage())
+console.log(mas)
+
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
